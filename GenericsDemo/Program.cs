996cd09d104f6fa9d02a956369838bf694c034c5
BotBandi <andras.meszaros413@gmail.com>
2022-01-24 20:06:33 +0100

    Add project files.

---
+++ GenericsDemo/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace GenericsDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DemonstrateTextFileStorage();
+
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+
+        private static void DemonstrateTextFileStorage()
+        {
+
+        }
+    }
+}
